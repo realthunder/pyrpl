efa6db64b86eb81a8b2af966ce6ca6f021ac8aa7
Zheng Lei <realthunder.dev@gmail.com>
2025-04-09 09:37:41 +0800

    Fix fpga loading error

--- pyrpl/fpga/red_pitaya_vivado.tcl
+++ pyrpl/fpga/red_pitaya_vivado.tcl
@@ -157,12 +157,15 @@ write_bitstream -force $path_out/red_pitaya.bit
 # generate the .bin file for flashing via 'cat red_pitaya.bin > /dev/xdevcfg'
 ################################################################################
 
-write_bitstream -force -bin_file  red_pitaya
-# exec bootgen -image $path_out/red_pitaya.bif -arch zynq -process_bitstream bin -o red_pitaya.bin -w
+# This is not working
+## write_bitstream -force -bin_file  red_pitaya
 
-# set_property BITSTREAM.GENERAL.COMPRESS FALSE [current_design]
-# write_bitstream -force $path_out/red_pitaya_uncompressed.bit
-# write_cfgmem -force -format BIN -size 4 -interface SMAPx32 -disablebitswap -loadbit "up 0x0 $path_out/red_pitaya_uncompressed.bit" red_pitaya.bin
+# This may works, but need bif file
+## exec bootgen -image $path_out/red_pitaya.bif -arch zynq -process_bitstream bin -o red_pitaya.bin -w
+
+set_property BITSTREAM.GENERAL.COMPRESS FALSE [current_design]
+write_bitstream -force $path_out/red_pitaya_uncompressed.bit
+write_cfgmem -force -format BIN -size 4 -interface SMAPx32 -disablebitswap -loadbit "up 0x0 $path_out/red_pitaya_uncompressed.bit" red_pitaya.bin
 
 ################################################################################
 # generate system definition
