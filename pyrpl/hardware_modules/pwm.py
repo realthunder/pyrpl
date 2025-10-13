e5a80c0aaf1d00df6dba612852b393a40e8df3b9
Zheng Lei <realthunder.dev@gmail.com>
2025-10-13 13:27:17 +0800

    Fix pwm module input setting restore

--- pyrpl/hardware_modules/pwm.py
+++ pyrpl/hardware_modules/pwm.py
@@ -36,8 +36,8 @@ class Pwm(DspModule):
                                                 pwm3='in2')[name])
         # because pwm's input is using adc-input's plug
         self.name = name
-        with self.do_setup:
-            self.input ='off'
+        #  with self.do_setup:
+        #      self.input ='off'
     """
     def __init__(self, client, name, parent): # name is 'pwm0' or 'pwm1'
         pwm_to_module = dict(pwm1='adc1', pwm2='adc2')
