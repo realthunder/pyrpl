af7928339ac5ae3fa2e663b308ef086e43b2ef5f
Zheng, Lei <realthunder.dev@gmail.com>
2025-12-08 22:49:42 +0800

    WIP

--- pyrpl/fpga/rtl/red_pitaya_scope.v
+++ pyrpl/fpga/rtl/red_pitaya_scope.v
@@ -470,27 +470,29 @@ end else begin
 end
 
 
-reg  [ 32-1:  0]fft_hist[0:(1<<HSZ)-1] ;
-reg  [ 32-1:  0]fft_hist_rdata         ;
-reg  [ HSZ-1: 0]fft_hist_raddr         ;
-reg  [ HSZ-1: 0]fft_hist_raddr2        ;
-reg  [ HSZ-1: 0]fft_hist_rp            ;
-reg  [ HSZ-1: 0]fft_hist_rp_next       ;
-reg  [ 32-1:  0]_fft_hist_last         ;
-reg  [ 32-1:  0]fft_hist_last          ;
-reg  [ HSZ-1: 0]fft_hist_length        ;
-reg  [ RSZ-1: 0]fft_peak_start         ;
-wire [ RSZ-1: 0]fft_peak_idx           ;
-wire [ 16-1:  0]fft_peak               ;
-wire [ RSZ-1: 0]fft_peak2_idx          ;
-wire [ 16-1:  0]fft_peak2              ;
-reg  [ 16-1:  0]fft_threshold_k        ;
-reg             fft_peak_ready_last    ;
-wire [ RSZ-1: 0]fft_peak_rp            ;
-wire            fft_peak_data_valid    ;
-wire [ 16-1:  0]fft_peak_data          ;
-wire [ 16-1:  0]fft_peak_data_abs      ;
-reg  [ 16-1:  0]fft_peak_minimum       ;
+reg  [ 32-1:  0]   fft_hist[0:(1<<HSZ)-1] ;
+reg  [ 32-1:  0]   fft_hist_rdata         ;
+reg  [ HSZ-1: 0]   fft_hist_raddr         ;
+reg  [ HSZ-1: 0]   fft_hist_raddr2        ;
+reg  [ HSZ-1: 0]   fft_hist_rp            ;
+reg  [ HSZ-1: 0]   fft_hist_rp_next       ;
+reg  [ 32-1:  0]   _fft_hist_last         ;
+reg  [ 32-1:  0]   fft_hist_last          ;
+reg  [ HSZ-1: 0]   fft_hist_length        ;
+reg  [ RSZ-1: 0]   fft_peak_start         ;
+wire [ RSZ-1: 0]   fft_peak_idx           ;
+wire [ 16-1:  0]   fft_peak               ;
+wire [ RSZ-1: 0]   fft_peak2_idx          ;
+wire [ 16-1:  0]   fft_peak2              ;
+wire [ RSZ+16-1: 0]fft_sum                ;
+wire [ RSZ-1: 0]   fft_count              ;
+reg  [ 16-1:  0]   fft_threshold_k        ;
+reg                fft_peak_ready_last    ;
+wire [ RSZ-1: 0]   fft_peak_rp            ;
+wire               fft_peak_data_valid    ;
+wire [ 16-1:  0]   fft_peak_data          ;
+wire [ 16-1:  0]   fft_peak_data_abs      ;
+reg  [ 16-1:  0]   fft_peak_minimum       ;
 
 assign fft_peak_rp = fft_rp;
 // assign fft_peak_data = fft_buf[fft_peak_rp];
@@ -559,6 +561,8 @@ peak_detector peak_detector_i (
     .peak           (fft_peak),
     .peak2_idx      (fft_peak2_idx),
     .peak2          (fft_peak2),
+    .sum            (fft_sum),
+    .count          (fft_count),
     .ready          (fft_peak_ready)
 );
 
@@ -1143,6 +1147,8 @@ end else begin
      20'h00044 : begin sys_ack <= sys_en;          sys_rdata <= fft_peak_minimum                    ; end
      20'h00048 : begin sys_ack <= sys_en;          sys_rdata <= fft_hist_last                       ; end
      20'h0004C : begin sys_ack <= sys_en;          sys_rdata <= {fft_peak2, fft_peak}               ; end
+     20'h00050 : begin sys_ack <= sys_en;          sys_rdata <= fft_sum                             ; end
+     20'h00054 : begin sys_ack <= sys_en;          sys_rdata <= fft_count                           ; end
 
      /*
      20'h00030 : begin sys_ack <= sys_en;          sys_rdata <= {{32-18{1'b0}}, set_a_filt_aa}      ; end
