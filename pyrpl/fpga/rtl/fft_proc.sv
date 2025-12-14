a885ebfa0f505ca19572f504c7d02db8f6bad75b
Zheng, Lei <realthunder.dev@gmail.com>
2025-12-14 18:07:05 +0800

    WIP

--- pyrpl/fpga/rtl/fft_proc.sv
+++ pyrpl/fpga/rtl/fft_proc.sv
@@ -6,7 +6,6 @@ module fft_proc #(
 )(
   input logic             clk_i,
   input logic             rstn_i,
-  input logic             sync_rst_i,
   input logic  [ 14-1: 0] data_i,
   input logic             dvalid_i, 
   input logic             trig_i,
@@ -21,6 +20,7 @@ module fft_proc #(
 
   input logic  [FSZ-1: 0] fft_raddr_i,
   output logic [ 16-1: 0] fft_rdata_o,
+  output logic [ FSZ-1:0] fft_wp_last,
 
   input logic  [ HSZ-1:0] fft_hist_raddr_i,
   output logic [ 32-1: 0] fft_hist_rdata_o,
@@ -32,11 +32,12 @@ module fft_proc #(
   output logic [  4-1: 0] fft_peak_state,
   output logic [ 32-1: 0] fft_peak_indices,
   output logic [ 32-1: 0] fft_peaks,
-  output logic [ 32-1: 0] fft_frame_cnt
+  output logic [ 32-1: 0] fft_frame_cnt,
+  output logic [ 32-1: 0] fft_we_cnt
 );
 
-logic [ 16-1: 0] fft_data_last;
-logic [ 32-1: 0] fft_we_cnt;
+// logic [ 32-1: 0] fft_we_cnt;
+
 logic [ 14-1: 0] fft_queue[0:(1<<QSZ)-1];
 logic [ 14-1: 0] fft_last_data;
 logic [ 14-1: 0] fft_data;
@@ -44,7 +45,6 @@ logic [ QSZ-1:0] fft_q_wp;
 logic [ QSZ-1:0] fft_q_rp;
 logic [ 14-1: 0] fft_data_i;
 logic [ 16-14:0] fft_data_ext;
-logic            fft_rstn_i;
 logic            fft_saxi_last;
 logic            fft_saxi_rdy;
 logic            fft_saxi_valid;
@@ -55,26 +55,24 @@ logic [ 16-1:  0]   fft_buf [0:(1<<FSZ)-1];
 logic [ HSZ-1: 0]   fft_hist_wp;
 logic [ HSZ-1: 0]   fft_hist_wp_next;
 logic [ 32-1:  0]   fft_peak_last_indices;
-logic [ 32-1:  0]   fft_peak_last;
 logic [ FSZ-1: 0]   fft_peak_idx;
 logic [ 16-1:  0]   fft_peak;
 logic [ FSZ-1: 0]   fft_peak2_idx;
 logic [ 16-1:  0]   fft_peak2;
-logic [ FSZ+16-1: 0]_fft_sum;
+logic [ FSZ+16-1:0] _fft_sum;
 logic [ FSZ: 0]     _fft_count;
-logic                fft_peak_ready_last;
+logic               fft_peak_ready_last;
 logic [ FSZ-1: 0]   fft_peak_rp;
 logic               fft_peak_data_valid;
 logic [ 16-1:  0]   fft_peak_data;
 logic [ 16-1:  0]   fft_peak_data_abs;
-logic [ 32-1: 0] _fft_peak_indices;
+logic [ 32-1: 0]    _fft_peak_indices;
 
-logic [ 16-1:  0]fft_maxi_msb;
-logic [ 16-1:  0]fft_maxi_lsb;
-logic            fft_maxi_valid;
-logic            fft_maxi_last;
-logic [ FSZ-1: 0]fft_rp_last;
-logic [ FSZ-1: 0]fft_rp;
+logic [ 16-1:  0]   fft_maxi_msb;
+logic [ 16-1:  0]   fft_maxi_lsb;
+logic               fft_maxi_valid;
+logic               fft_maxi_last;
+logic [ FSZ-1: 0]   fft_wp;
 
 logic [ HSZ-1: 0]   fft_hist_raddr1;
 logic [ HSZ-1: 0]   fft_hist_raddr2;
@@ -86,7 +84,6 @@ assign fft_data_ext = {16-14{fft_data_i[13]}};
 assign fft_saxi_last = fft_we_cnt == 1;
 assign fft_saxi_valid = fft_we_cnt > 0 && fft_we_cnt <= 2**FSZ;
 assign fft_done = fft_peak_ready_last && fft_we_cnt==0;
-assign fft_rstn_i = rstn_i && !sync_rst_i;
 assign fft_data = dvalid_i ? data_i : fft_last_data;
 
 always @(posedge clk_i) begin
@@ -100,12 +97,12 @@ always @(posedge clk_i) begin
 end
 
 always @(posedge clk_i)
-if (fft_rstn_i == 1'b0) begin
+if (rstn_i == 1'b0) begin
     fft_we_cnt <= 0;
     fft_q_wp <= 0;
     fft_q_rp <= 0;
     fft_frame_cnt <= 0 ;
-    fft_data_last <= 0 ;
+    fft_last_data <= 0 ;
 end else begin
     if (fft_frame_start)
         fft_frame_cnt = fft_frame_cnt + 1 ;
@@ -113,7 +110,7 @@ end else begin
     fft_queue[fft_q_wp] <= fft_data ;
     fft_q_wp <= fft_q_wp + 1;
     if (dvalid_i)
-        fft_data_last <= data_i;
+        fft_last_data <= data_i;
 
     if (trig_i && fft_done) begin
         fft_we_cnt <= set_dly;
@@ -130,31 +127,30 @@ end else begin
 end
 
 always @(posedge clk_i)
-if (fft_rstn_i == 1'b0) begin
-    fft_rp <= 0;
-    fft_rp_last <= 0;
+if (rstn_i == 1'b0) begin
+    fft_wp <= 0;
+    fft_wp_last <= 0;
 end else if (fft_maxi_valid) begin
-    fft_buf[fft_rp] <= fft_maxi_lsb;
+    fft_buf[fft_wp] <= fft_maxi_lsb;
     if (fft_maxi_last) begin
-        fft_rp_last <= fft_rp;
-        fft_rp <= 0;
+        fft_wp_last <= fft_wp;
+        fft_wp <= 0;
     end else
-        fft_rp = fft_rp + 1;
+        fft_wp = fft_wp + 1;
 end
 
-assign fft_peak_rp = fft_rp;
+assign fft_peak_rp = fft_wp;
 assign fft_peak_data = fft_maxi_lsb;
 assign fft_peak_data_abs = fft_peak_data[16-1] ? -fft_peak_data : fft_peak_data;
 assign fft_peak_data_valid = fft_maxi_valid && fft_peak_rp>=fft_peak_start && fft_peak_rp[FSZ-1]==0 && fft_peak_data_abs>fft_peak_minimum;
 assign _fft_peak_indices = {{16-FSZ{1'b0}}, fft_peak2_idx, {16-FSZ{1'b0}}, fft_peak_idx};
 
 always @(posedge clk_i) begin
-    if (fft_rstn_i == 1'b0) begin
+    if (rstn_i == 1'b0) begin
         fft_hist_wp <= 0;
         fft_hist_wp_next <= 1;
         fft_peak_last_indices <= 0;
-        fft_peak_last <= 0;
-        fft_peak_ready_last <= 0;
+        fft_peak_ready_last <= 1;
     end else begin
         fft_peak_ready_last <= fft_peak_ready;
         if (!fft_peak_ready_last && fft_peak_ready) begin
@@ -177,7 +173,7 @@ end
 
 peak_detector #(.SSZ(FSZ), .DSZ(16)) peak_detector_i (
     .clk            (clk_i),
-    .resetn         (fft_rstn_i),
+    .resetn         (rstn_i),
     .data_valid     (fft_peak_data_valid),
     .data_in        (fft_peak_data_abs),
     .data_index     (fft_peak_rp),
@@ -206,7 +202,7 @@ fft_wrapper fft_i (
    .S_AXIS_DATA_0_tready        (fft_saxi_rdy     ),
    .S_AXIS_DATA_0_tvalid        (fft_saxi_valid   ),
    .aclk_0                      (clk_i            ),
-   .aresetn_0                   (fft_rstn_i       ),
+   .aresetn_0                   (rstn_i           ),
    .event_data_in_channel_halt_0(fft_in_halt      ),
    .event_data_out_channel_halt_0(fft_out_halt    ),
    .event_status_channel_halt_0 (fft_status_halt  ),
