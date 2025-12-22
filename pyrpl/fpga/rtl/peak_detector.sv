edc60abb30930a4c631fed4f99994aa914e74aa8
Zheng, Lei <realthunder.dev@gmail.com>
2025-12-22 23:02:10 +0800

    WIP

--- pyrpl/fpga/rtl/peak_detector.sv
+++ pyrpl/fpga/rtl/peak_detector.sv
@@ -42,13 +42,13 @@ module peak_detector #(
 //      (peak * N - mean*N)^2  >  k^2 * (N * N * mean_of_square - square_of_mean * N * N)
 //         (peak * N - sum)^2  >  k^2 * ( N * sum_of_square - square_of_sum)
 
-localparam PIPLINE = 4-1;
+localparam PIPELINE = 4-1;
 localparam S_IDLE = 0;
 localparam S_STREAM = 1;
 localparam S_DETECT = 2;
-localparam S_DETECT1 = S_DETECT+PIPLINE+1;
-localparam S_DETECT2 = S_DETECT1+PIPLINE+1;
-localparam S_DETECT3 = S_DETECT2+PIPLINE+1;
+localparam S_DETECT1 = S_DETECT+PIPELINE+1;
+localparam S_DETECT2 = S_DETECT1+PIPELINE+1;
+localparam S_DETECT3 = S_DETECT2+PIPELINE+1;
 
 logic [8-1: 0] current_state;
 
@@ -56,26 +56,26 @@ logic [SSZ+DSZ-1:0] sum; // sum of all data. Max value: 2^SSZ * 2^DSZ
 logic [SSZ:0] count;
 
 // sum of square of each data
-logic [SSZ+DSZ*2-1:0] sum_sq[PIPLINE: 0];      // Max value: 2^SSZ * 2^DSZ * 2^DSZ
+logic [SSZ+DSZ*2-1:0] sum_sq[PIPELINE: 0];      // Max value: 2^SSZ * 2^DSZ * 2^DSZ
 
 // square of sum
-logic [(SSZ+DSZ)*2-1:0] S_sq [PIPLINE: 0]; // n-stage piplining
+logic [(SSZ+DSZ)*2-1:0] S_sq [PIPELINE: 0]; // n-stage piplining
 // N * sum_of_square
-logic [(SSZ+DSZ)*2-1:0] N_S2 [PIPLINE: 0]; // n-stage piplining
+logic [(SSZ+DSZ)*2-1:0] N_S2 [PIPELINE: 0]; // n-stage piplining
 
 // Scaled variance, i.e. N^2 * Variance = (N * sum_of_squrae - square_of_sum)
 logic [(SSZ+DSZ)*2-1:0] V_scaled;
-assign V_scaled = N_S2[PIPLINE] - S_sq[PIPLINE];
+assign V_scaled = N_S2[PIPELINE] - S_sq[PIPELINE];
 
 // peak * N - sum
-logic [SSZ+DSZ-1:0] scaled_diff [PIPLINE: 0];
+logic [SSZ+DSZ-1:0] scaled_diff [PIPELINE: 0];
 // peak2 * N - sum
-logic [SSZ+DSZ-1:0] scaled_diff2 [PIPLINE: 0]; 
+logic [SSZ+DSZ-1:0] scaled_diff2 [PIPELINE: 0]; 
 
-logic [(SSZ+DSZ)*2-1:0] scaled_diff_sq  [PIPLINE: 0]; // scaled_diff ^ 2, n-stage piplining
-logic [(SSZ+DSZ)*2-1:0] scaled_diff2_sq [PIPLINE: 0]; // scaled_diff2 ^2, n-stage piplining
+logic [(SSZ+DSZ)*2-1:0] scaled_diff_sq  [PIPELINE: 0]; // scaled_diff ^ 2, n-stage piplining
+logic [(SSZ+DSZ)*2-1:0] scaled_diff2_sq [PIPELINE: 0]; // scaled_diff2 ^2, n-stage piplining
 
-logic [(SSZ+DSZ)*2-1:0] threshold [PIPLINE: 0];
+logic [(SSZ+DSZ)*2-1:0] threshold [PIPELINE: 0];
 
 assign ready = current_state==S_IDLE;
 
@@ -98,7 +98,7 @@ end else begin
         peak2 <= 0;
         count <= 0;
         sum <= 0;
-        for (i=0; i<=PIPLINE; i=i+1) begin
+        for (i=0; i<=PIPELINE; i=i+1) begin
             sum_sq[i] <= 0;
         end
         current_state <= S_STREAM;
@@ -117,24 +117,30 @@ end else begin
             end
             sum <= sum + data_in;
             sum_sq[0] <= data_in * data_in;
-            for (i=0; i<PIPLINE; i=i+1) begin
-                sum_sq[i+1] <= sum_sq[i+1] + sum_sq[i];
+            for (i=0; i<PIPELINE-1; i=i+1) begin
+                sum_sq[i+1] <= sum_sq[i];
             end
+            sum_sq[PIPELINE] <= sum_sq[PIPELINE] + sum_sq[PIPELINE-1];
             count <= count + 1;
         end
+
     end else if (current_state >= S_DETECT && current_state < S_DETECT1) begin
-        for (i=0; i<PIPLINE; i=i+1) begin
+        for (i=0; i<PIPELINE-1; i=i+1) begin
             sum_sq[i+1] <= sum_sq[i+1] + sum_sq[i];
         end
+        sum_sq[PIPELINE] <= sum_sq[PIPELINE] + sum_sq[PIPELINE-1];
+
+        current_state <= current_state + 1;
+
     end else if (current_state >= S_DETECT1 && current_state < S_DETECT2) begin
         S_sq[0] <= sum * sum; // S^2
-        N_S2[0] <= count * sum_sq[PIPLINE]; // N * S2
+        N_S2[0] <= count * sum_sq[PIPELINE]; // N * S2
 
         // TODO: do we need to worry about signess?
         scaled_diff[0] <= peak * count - sum;
         scaled_diff2[0] <= peak2 * count - sum;
 
-        for (i=0; i<PIPLINE; i=i+1) begin
+        for (i=0; i<PIPELINE; i=i+1) begin
             S_sq[i+1] <= S_sq[i];
             N_S2[i+1] <= N_S2[i];
             scaled_diff[i+1] <= scaled_diff[0];
@@ -145,10 +151,10 @@ end else begin
 
     end else if (current_state >= S_DETECT2 && current_state < S_DETECT3) begin
         threshold[0] <= threshold_k_sq * V_scaled;
-        scaled_diff_sq[0] <= scaled_diff[PIPLINE] * scaled_diff[PIPLINE];
-        scaled_diff2_sq[0] <= scaled_diff2[PIPLINE] * scaled_diff2[PIPLINE];
+        scaled_diff_sq[0] <= scaled_diff[PIPELINE] * scaled_diff[PIPELINE];
+        scaled_diff2_sq[0] <= scaled_diff2[PIPELINE] * scaled_diff2[PIPELINE];
 
-        for (i=0; i<PIPLINE; i=i+1) begin
+        for (i=0; i<PIPELINE; i=i+1) begin
             threshold[i+1] <= threshold[i];
             scaled_diff_sq[i+1] <= scaled_diff_sq[i];
             scaled_diff2_sq[i+1] <= scaled_diff2_sq[i];
@@ -157,10 +163,10 @@ end else begin
         current_state <= current_state + 1;
 
     end else if (current_state == S_DETECT3) begin
-        if (scaled_diff_sq[PIPLINE] < threshold[PIPLINE]) begin
+        if (scaled_diff_sq[PIPELINE] < threshold[PIPELINE]) begin
             peak_idx <= 0;
             peak2_idx <= 0;
-        end else if (scaled_diff2_sq[PIPLINE] < threshold[PIPLINE])
+        end else if (scaled_diff2_sq[PIPELINE] < threshold[PIPELINE])
             peak2_idx <= 0;
         sum_o <= sum;
         count_o <= count;
